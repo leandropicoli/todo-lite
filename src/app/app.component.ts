34616ecba27ea886542b730ea367fda4c12589da
Leandro Picoli <leandropicoli@live.com>
2020-09-22 08:28:35 -0300

    Create functions to set todo as done and undone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,11 +23,11 @@ export class AppComponent {
     }
   }
 
-  markAsDone() {
-
+  markAsDone(todo: Todo) {
+    todo.done = true;
   }
 
-  markAsUndone() {
-
+  markAsUndone(todo: Todo) {
+    todo.done = false;
   }
 }
