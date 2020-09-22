d74aa59f4ee353faeb994d8268a2bc3126f99937
Leandro Picoli <leandropicoli@live.com>
2020-09-22 08:19:09 -0300

    Testing ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   constructor() {
     this.todos.push(new Todo(1, 'study angular', false));
     this.todos.push(new Todo(2, 'study csharp', false));
-    this.todos.push(new Todo(3, 'go shopping', false));
+    this.todos.push(new Todo(3, 'go shopping', true));
   }
 
 }
